aeabcf58b03f2ec80f1d42eab1e1f9aa5c26f152
codefan <codefan@centit.com>
2016-06-02 10:12:53 +0000

    git-svn-id: https://fileserver.centit.com/svn/centit/products/centit-meta-form@144198 11746a9b-a569-1649-8427-d29ac4cc1c6a

--- src/main/java/com/centit/metaform/fromaccess/ModelOperation.java
+++ src/main/java/com/centit/metaform/fromaccess/ModelOperation.java
@@ -1,5 +1,45 @@
 package com.centit.metaform.fromaccess;
 
 public class ModelOperation {
+	private String modelCode;
+	private String operation;
+	private String label;
+	private String url;
+	
+	public ModelOperation(){
+		
+	}
 
+	public String getModelCode() {
+		return modelCode;
+	}
+
+	public void setModelCode(String modelCode) {
+		this.modelCode = modelCode;
+	}
+
+	public String getOperation() {
+		return operation;
+	}
+
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
 }
