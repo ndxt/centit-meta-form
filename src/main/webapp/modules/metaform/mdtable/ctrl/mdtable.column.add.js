a5f38b8622628e651efdeba6570ac2643c9b9ad4
oldsvnuser <test2@centit.com>
2016-06-20 08:13:59 +0000

    git-svn-id: https://fileserver.centit.com/svn/centit/products/centit-meta-form@146232 11746a9b-a569-1649-8427-d29ac4cc1c6a

--- src/main/webapp/modules/metaform/mdtable/ctrl/mdtable.column.add.js
+++ src/main/webapp/modules/metaform/mdtable/ctrl/mdtable.column.add.js
@@ -10,7 +10,7 @@ define(function(require) {
 			}
 			var form=this.parent.panel.find('form');
 			var value=form.form('value');
-			table.datagrid('appendRow', $.extend({tableId:value.tableId}, this.object));
+			table.datagrid('appendRow', $.extend({tableId:this.parent.data.tableId}, this.object));
 			var index = table.datagrid('getRows').length - 1;
 			table.datagrid('selectRow', index);
 			table.cdatagrid('beginEdit', index, 'dataCode');
