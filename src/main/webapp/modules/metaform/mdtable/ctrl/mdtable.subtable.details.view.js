32a18f29b69d9b536770719f8d29d294d948a7e0
oldsvnuser <test2@centit.com>
2016-06-29 02:35:57 +0000

    git-svn-id: https://fileserver.centit.com/svn/centit/products/centit-meta-form@147613 11746a9b-a569-1649-8427-d29ac4cc1c6a

--- src/main/webapp/modules/metaform/mdtable/ctrl/mdtable.subtable.details.view.js
+++ src/main/webapp/modules/metaform/mdtable/ctrl/mdtable.subtable.details.view.js
@@ -11,7 +11,7 @@ define(function(require) {
 		// @override
 		this.load = function(panel, data) {
 			var subtableDetailsTable=panel.find("#mdtable_subtable_details");
-			var mdtable_columns=this.parent.panel.find("#mdtable_columns");
+			var mdtable_columns=this.parent.panel.find("#mdtable_columns_view");
 			var mdtablecolumns=mdtable_columns.datagrid('getRows');
 			var list=[];
 			$.ajax({
@@ -28,6 +28,7 @@ define(function(require) {
 			
 			subtableDetailsTable.datagrid({
 				height:'100%',
+				editable:false,
 				width:'100%',
 				frozenColumns:[[
 				          {field:'parentColumnName',title:'parentColumnName',width: 180,
@@ -52,67 +53,9 @@ define(function(require) {
 				          {field:'',title:'childColumnName'}
 				          
 				          
-				         ]],
-		         
-		         toolbar:[
-							{
-							    text:'新增',
-							    iconCls:'icon-add',
-							    handler:function(){
-							    	if (editRow != undefined) {
-							    		subtableDetailsTable.datagrid("endEdit", editRow);
-							    	}
-							    	if (editRow == undefined) {
-							    		var obj={};
-								    	subtableDetailsTable.datagrid('appendRow', obj);
-										var index = subtableDetailsTable.datagrid('getRows').length - 1;
-										editRow=index;
-										subtableDetailsTable.datagrid('beginEdit', index);
-				                    }
-							    	
-							    	
-							    	
-									
-							    	}
-							    
-							},
-							{
-							    text:'删除',
-							    iconCls:'icon-remove',
-							    handler:function(){
-							    	if (editRow != undefined) {
-							    		subtableDetailsTable.datagrid("endEdit", editRow);
-							    	}
-							    	
-							    	var rows = subtableDetailsTable.datagrid("getSelections");
-									 //选择要删除的行
-									 if (rows.length > 0) {
-										for (var i = 0; i < rows.length; i++) {
-													 var index = subtableDetailsTable.datagrid('getRowIndex', rows[i]);
-													 subtableDetailsTable.datagrid('deleteRow', index);
-										 }
-									 }
-									 else{
-										 $.messager.alert("提示", "请选择要删除的行", "error");
-									 }
-									
-							    }
-							    	
-							    
-							}
-		                  ],
-	                  onDblClickRow: function (rowIndex, rowData) {
-	                          if (editRow != undefined) {
-	                        	  subtableDetailsTable.datagrid("endEdit", editRow);
-	                          }
-	                          if (editRow == undefined) {
-	                        	  subtableDetailsTable.datagrid("beginEdit", rowIndex);
-	                              editRow = rowIndex;
-	                          } 
-	                  },
-	                  onAfterEdit: function (rowIndex, rowData, changes) {
-	                      editRow = undefined;
-	                  },
+				         ]]
+				      
+	                  
 			}).datagrid('loadData',$.extend([], data.relationDetails));
 		};
 		
--- src/main/webapp/modules/metaform/mdtable/ctrl/mdtable.view.js
+++ src/main/webapp/modules/metaform/mdtable/ctrl/mdtable.view.js
@@ -2,11 +2,13 @@ define(function(require) {
 	var Config = require('config');
 	var Core = require('core/core');
 	var Page = require('core/page');
-	
+	var MdtableSubtableDetailsView=require("./mdtable.subtable.details.view");
 	var MdTableView = Page.extend(function() {
 		
 		var _self = this;
-		
+		this.injecte([
+		              new MdtableSubtableDetailsView('mdtable_subtable_details_view')
+		        	]);
 		// @override
 		this.load = function(panel, data) {
 			var form = panel.find('form');
@@ -14,6 +16,11 @@ define(function(require) {
 			mdtable_columns.cdatagrid({
 				controller:_self
 			})
+			
+			var mdtable_subtables=panel.find("#mdtable_subtables_view");
+			mdtable_subtables.cdatagrid({
+				controller:_self
+			});
 			Core.ajax(Config.ContextPath+'service/metaform/mdtable/draft/'+data.tableId, {
 				type: 'json',
 				method: 'get' 
@@ -24,6 +31,7 @@ define(function(require) {
 					.form('disableValidation')
 					.form('focus');
 				mdtable_columns.datagrid('loadData',data.mdColumns);
+				mdtable_subtables.datagrid('loadData',data.mdRelations);
 			});
 		};
 	});
