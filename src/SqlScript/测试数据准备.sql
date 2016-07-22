96c7ee2f4ead98417276464486626c1076cc2867
accounts <accounts@centit.com>
2016-07-22 01:57:02 +0000

    git-svn-id: https://fileserver.centit.com/svn/centit/products/centit-meta-form@151558 11746a9b-a569-1649-8427-d29ac4cc1c6a

--- src/SqlScript/测试数据准备.sql
+++ src/SqlScript/测试数据准备.sql
@@ -1,23 +1,38 @@
---测试
+
 insert into M_Model_Data_Field (Model_Code,column_Name,Column_Type,Access_Type,Input_Type,
 Reference_Type,Reference_Data,Input_Hint)
 select 'NEW_OPPORTUNITIE', column_Name , 'T', 'N','input',
 Reference_Type,Reference_Data,column_comment
-from F_META_COLUMN where table_id='105';
+from F_META_COLUMN where table_id=105;
+
+
+insert into M_Model_Data_Field (Model_Code,column_Name,Column_Type,Access_Type,Input_Type,
+Reference_Type,Reference_Data,Input_Hint,FIELD_HEIGHT,Field_Width)
+select 'OPPORT_CONTRACTS', column_Name , 'T', 'N','input',
+Reference_Type,Reference_Data,column_comment,1,6
+from F_META_COLUMN where table_id=62;
+
+
+insert into M_Model_Data_Field (Model_Code,column_Name,Column_Type,Access_Type,Input_Type,
+Reference_Type,Reference_Data,Input_Hint,FIELD_HEIGHT,Field_Width)
+select 'OPPORT_EXPENSES', column_Name , 'T', 'N','input',
+Reference_Type,Reference_Data,column_comment,1,6
+from F_META_COLUMN where table_id=69;
+
 
 select * from F_META_TABLE;
 
 update M_Model_Data_Field
 set input_TYPE='select',
-	reference_Type='3',
-	reference_Data='[{value:1,name:"行业固定前期事务"},{value:2,name:"一般前期事务"}]'
+  reference_Type='3',
+  reference_Data='[{value:1,name:"行业固定前期事务"},{value:2,name:"一般前期事务"}]'
 where model_code= 'NEW_OPPORTUNITIE' and column_name='L_OPP_TYPE'
 /
 
 update M_Model_Data_Field
 set input_TYPE='select',
-	reference_Type='3',
-	reference_Data='[{value:1,name:"进行中事务"},{value:2,name:"关闭的事务"}]'
+  reference_Type='3',
+  reference_Data='[{value:1,name:"进行中事务"},{value:2,name:"关闭的事务"}]'
 where model_code= 'NEW_OPPORTUNITIE' and column_name='L_OPP_STATE'
 /
 
@@ -40,44 +55,46 @@ where table_id= 105 and column_Name='L_OPP_STATE'
 
 update M_Model_Data_Field
 set input_TYPE='select',
-	reference_Type='9',
-	reference_Data='unitCode'
+  reference_Type='9',
+  reference_Data='unitCode'
 where model_code= 'NEW_OPPORTUNITIE' and column_name='L_OWNER_DEP_ID'
 /
 
 update F_META_COLUMN
 set reference_Type='9',
-	reference_Data='unitCode'
+  reference_Data='unitCode'
 where table_id= 105 and column_Name='L_OWNER_DEP_ID'
 /
 
 update F_META_COLUMN
 set reference_Type='3',
-	reference_Data='[{value:1,name:"行业固定前期事务"},{value:2,name:"一般前期事务"}]'
+  reference_Data='[{value:1,name:"行业固定前期事务"},{value:2,name:"一般前期事务"}]'
 where table_id= 105 and column_Name='L_OPP_TYPE'
 /
 
 update F_META_COLUMN
 set reference_Type='3',
-	reference_Data='[{value:1,name:"进行中事务"},{value:2,name:"关闭的事务"}]'
+  reference_Data='[{value:1,name:"进行中事务"},{value:2,name:"关闭的事务"}]'
 where table_id= 105 and  column_name='L_OPP_STATE'
 /
 
 
 update F_PENDING_META_COLUMN
 set reference_Type='9',
-	reference_Data='unitCode'
+  reference_Data='unitCode'
 where table_id= 105 and column_Name='L_OWNER_DEP_ID'
 /
 
 update F_PENDING_META_COLUMN
 set reference_Type='3',
-	reference_Data='[{value:1,name:"行业固定前期事务"},{value:2,name:"一般前期事务"}]'
+  reference_Data='[{value:1,name:"行业固定前期事务"},{value:2,name:"一般前期事务"}]'
 where table_id= 105 and column_Name='L_OPP_TYPE'
 /
 
 update F_PENDING_META_COLUMN
 set reference_Type='3',
-	reference_Data='[{value:1,name:"进行中事务"},{value:2,name:"关闭的事务"}]'
+  reference_Data='[{value:1,name:"进行中事务"},{value:2,name:"关闭的事务"}]'
 where table_id= 105 and  column_name='L_OPP_STATE'
 /
+
+
