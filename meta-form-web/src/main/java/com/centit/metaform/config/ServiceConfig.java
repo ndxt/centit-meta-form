ab26a349550ccf08d3119fc10a2c37f2d2d7336f
zou_wy <zou_wy@centit.com>
2018-12-07 10:07:51 +0800

    注册PasswordEncoder bean

--- meta-form-web/src/main/java/com/centit/metaform/config/ServiceConfig.java
+++ meta-form-web/src/main/java/com/centit/metaform/config/ServiceConfig.java
@@ -4,6 +4,10 @@ import com.centit.framework.config.SpringSecurityCasConfig;
 import com.centit.framework.config.SpringSecurityDaoConfig;
 import com.centit.framework.ip.app.config.IPOrStaticAppSystemBeanConfig;
 import com.centit.framework.jdbc.config.JdbcConfig;
+import com.centit.framework.security.model.CentitPasswordEncoder;
+import com.centit.framework.security.model.CentitPasswordEncoderImpl;
+import com.centit.framework.security.model.StandardPasswordEncoderImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -16,5 +20,10 @@ import org.springframework.context.annotation.Import;
         SpringSecurityDaoConfig.class,
         SpringSecurityCasConfig.class})
 public class ServiceConfig {
+
+    @Bean(name = "passwordEncoder")
+    public CentitPasswordEncoder centitPasswordEncoder(){
+        return new StandardPasswordEncoderImpl();
+    }
 
 }
