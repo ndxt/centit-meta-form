52194b732b4953314ffae4d50c500cbf2fb6a5cd
杨淮生 <codefan@centit.com>
2019-04-17 15:57:31 +0800

    重构元数据表单 增删改查操作

--- meta-form-module/src/main/java/com/centit/metaform/controller/MetaFormController.java
+++ meta-form-module/src/main/java/com/centit/metaform/controller/MetaFormController.java
@@ -2,13 +2,14 @@ package com.centit.metaform.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.centit.framework.common.ResponseData;
 import com.centit.framework.core.controller.BaseController;
 import com.centit.framework.core.controller.WrapUpResponseBody;
 import com.centit.framework.core.dao.PageQueryResult;
 import com.centit.metaform.po.MetaFormModel;
 import com.centit.metaform.service.MetaFormModelManager;
-import com.centit.product.dataopt.service.MetaObjectService;
+import com.centit.product.metadata.service.MetaObjectService;
 import com.centit.support.database.utils.PageDesc;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -37,6 +38,11 @@ public class MetaFormController extends BaseController {
     @Autowired
     private MetaObjectService metaObjectService;
 
+    private int runJSEvent(String js, Map<String, Object> object, String event){
+        return 1;
+    }
+
+
     @ApiOperation(value = "分页查询表单数据列表")
     @RequestMapping(value = "/{modelCode}/list", method = RequestMethod.GET)
     @WrapUpResponseBody
@@ -57,7 +63,7 @@ public class MetaFormController extends BaseController {
     }
 
     @ApiOperation(value = "获取一个数据，主键作为参数以key-value形式提交")
-    @RequestMapping(value = "/{modelCode}/get", method = RequestMethod.GET)
+    @RequestMapping(value = "/{modelCode}", method = RequestMethod.GET)
     @WrapUpResponseBody
     public Map<String, Object> getObject(@PathVariable String modelCode,
                                                HttpServletRequest request) {
@@ -72,6 +78,8 @@ public class MetaFormController extends BaseController {
     public ResponseData updateObject(@PathVariable String modelCode,
                                             @RequestBody String jsonString) {
         MetaFormModel model = metaFormModelManager.getObjectById(modelCode);
+        JSONObject object = JSON.parseObject(jsonString);
+        //runJSEvent(model.getExtendOptBean(), object, "update");
         metaObjectService.updateObject(model.getTableId(), JSON.parseObject(jsonString));
         return ResponseData.makeSuccessResponse();
     }
@@ -83,6 +91,58 @@ public class MetaFormController extends BaseController {
                                           @RequestBody String jsonString) {
         MetaFormModel model = metaFormModelManager.getObjectById(modelCode);
         metaObjectService.saveObject(model.getTableId(), JSON.parseObject(jsonString));
+        return ResponseData.makeSuccessResponse();
+    }
+
+    @ApiOperation(value = "删除表单数据")
+    @RequestMapping(value = "/{modelCode}", method = RequestMethod.DELETE)
+    @WrapUpResponseBody
+    public ResponseData deleteObject(@PathVariable String modelCode,
+                                     HttpServletRequest request) {
+        Map<String, Object> parameters = collectRequestParameters(request);
+        MetaFormModel model = metaFormModelManager.getObjectById(modelCode);
+        metaObjectService.deleteObject(model.getTableId(), parameters);
+        return ResponseData.makeSuccessResponse();
+    }
+
+    @ApiOperation(value = "获取一个数据带子表，主键作为参数以key-value形式提交")
+    @RequestMapping(value = "/{modelCode}/withChildren", method = RequestMethod.GET)
+    @WrapUpResponseBody
+    public Map<String, Object> getObjectWithChildren(@PathVariable String modelCode,
+                                         HttpServletRequest request) {
+        Map<String, Object> parameters = collectRequestParameters(request);
+        MetaFormModel model = metaFormModelManager.getObjectById(modelCode);
+        return metaObjectService.getObjectWithChildren(model.getTableId(), parameters, 1);
+    }
+
+    @ApiOperation(value = "修改表单数据带子表")
+    @RequestMapping(value = "/{modelCode}/withChildren", method = RequestMethod.PUT)
+    @WrapUpResponseBody
+    public ResponseData updateObjectWithChildren(@PathVariable String modelCode,
+                                     @RequestBody String jsonString) {
+        MetaFormModel model = metaFormModelManager.getObjectById(modelCode);
+        metaObjectService.mergeObjectWithChildren(model.getTableId(), JSON.parseObject(jsonString));
+        return ResponseData.makeSuccessResponse();
+    }
+
+    @ApiOperation(value = "新增表单数据带子表")
+    @RequestMapping(value = "/{modelCode}/withChildren", method = RequestMethod.POST)
+    @WrapUpResponseBody
+    public ResponseData saveObjectWithChildren(@PathVariable String modelCode,
+                                   @RequestBody String jsonString) {
+        MetaFormModel model = metaFormModelManager.getObjectById(modelCode);
+        metaObjectService.saveObjectWithChildren(model.getTableId(), JSON.parseObject(jsonString));
+        return ResponseData.makeSuccessResponse();
+    }
+
+    @ApiOperation(value = "删除表单数据带子表")
+    @RequestMapping(value = "/{modelCode}/withChildren", method = RequestMethod.DELETE)
+    @WrapUpResponseBody
+    public ResponseData deleteObjectWithChildren(@PathVariable String modelCode,
+                                     HttpServletRequest request) {
+        Map<String, Object> parameters = collectRequestParameters(request);
+        MetaFormModel model = metaFormModelManager.getObjectById(modelCode);
+        metaObjectService.deleteObjectWithChildren(model.getTableId(), parameters);
         return ResponseData.makeSuccessResponse();
     }
 
