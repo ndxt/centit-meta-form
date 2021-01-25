99aaa6cd7e73dae0389d62355e0b3dd2c2d09e2b
liu_cc <liu_cc@centit.com>
2021-01-25 18:03:43 +0800

    自定义表单，添加模板

---
+++ meta-form-module/src/main/java/com/centit/metaform/service/MetaFormTemplateService.java
@@ -0,0 +1,37 @@
+package com.centit.metaform.service;
+
+import com.alibaba.fastjson.JSONArray;
+import com.centit.metaform.po.MetaFormTemplate;
+import com.centit.support.database.utils.PageDesc;
+
+import java.util.Map;
+
+/**
+ * MetaFormTemplate  Service.
+ *
+ * @Date 2021/1/25
+ * 页面模板管理
+ */
+public interface MetaFormTemplateService {
+
+    /**
+     * 获取所有的页面模板
+     *
+     * @param filterMap 过滤条件
+     * @param pageDesc  分页描述
+     * @return
+     */
+    JSONArray listFormTemplate(Map<String, Object> filterMap, PageDesc pageDesc);
+
+    /**
+     * 根据id获取页面模板
+     *
+     * @param id
+     * @return
+     */
+    MetaFormTemplate getMetaFormTemplate(String id);
+
+    void saveMetaFormTemplate(MetaFormTemplate metaFormTemplate);
+
+    void deleteMetaFormTemplateById(String id);
+}
