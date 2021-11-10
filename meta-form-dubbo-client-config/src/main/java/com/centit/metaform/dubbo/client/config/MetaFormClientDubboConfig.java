89d8e88c5971e29ec8c3f8356e8030277ad516b0
zhou_c <zhou_c@centit.com>
2021-11-10 14:30:42 +0800

    修改包名

--- meta-form-dubbo-client-config/src/main/java/com/centit/metaform/dubbo/client/config/MetaFormClientDubboConfig.java
+++ meta-form-dubbo-client-config/src/main/java/com/centit/metaform/dubbo/client/config/MetaFormClientDubboConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.centit.metaform.dubbo.client.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
