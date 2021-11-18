9e5d09607907a7a60a4933a31cb2dcb4d6d6ac57
zhou_c <zhou_c@centit.com>
2021-11-18 15:11:11 +0800

    移除工作组dubbo服务

--- meta-form-web/src/main/java/com/centit/metaform/config/MetaformSpringMvcConfig.java
+++ meta-form-web/src/main/java/com/centit/metaform/config/MetaformSpringMvcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.FilterType;
 /**
  * Created by zou_wy on 2017/3/29.
  */
-@ComponentScan(basePackages = {"com.centit.metaform.controller","com.centit.application.controller","com.centit.product.oa.controller"},
+@ComponentScan(basePackages = {"com.centit.metaform.controller","com.centit.application.controller"},
         includeFilters = {@ComponentScan.Filter(type= FilterType.ANNOTATION,value= org.springframework.stereotype.Controller.class)},
         useDefaultFilters = false)
 public class MetaformSpringMvcConfig extends BaseSpringMvcConfig {
