a39497d9771f31c5c9623e46e8708ca75a780d5f
xue_qk <xue_qk@centit.com>
2021-12-29 16:14:34 +0800

    1.修改 login.dao.enable配置项 从nacos中获取

--- meta-form-web/src/main/java/com/centit/metaform/config/WebInitializer.java
+++ meta-form-web/src/main/java/com/centit/metaform/config/WebInitializer.java
@@ -1,21 +1,32 @@
 package com.centit.metaform.config;
 
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.centit.framework.common.SysParametersUtils;
 import com.centit.framework.config.SystemSpringMvcConfig;
 import com.centit.framework.config.WebConfig;
-import com.centit.support.file.PropertiesReader;
+import com.centit.support.algorithm.CollectionsOpt;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.WebApplicationInitializer;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import java.util.Properties;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * Created by zou_wy on 2017/3/29.
  */
 
 public class WebInitializer implements WebApplicationInitializer {
+    @SneakyThrows
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
+
+        findProperties(CollectionsOpt.createList("login.dao.enable","login.cas.enable"));
 
         String [] servletUrlPatterns = {"/system/*","/metadata/*","/metaform/*","/dbdesign/*"};
         WebConfig.registerSpringConfig(servletContext, ServiceConfig.class);
@@ -44,6 +55,45 @@ public class WebInitializer implements WebApplicationInitializer {
         WebConfig.registerSpringSecurityFilter(servletContext,servletUrlPatterns);
         //registerOpenSessionInViewFilter(servletContext);
 
+    }
+
+
+    /**
+     * 查找配置，优先从system.properties中获取，如果没有从nacos中获取
+     * @param keys 需要被提前查找的key
+     * @throws IOException
+     * @throws NacosException
+     */
+    private void findProperties(List<String> keys) throws IOException, NacosException {
+        HashMap<String, Object> map = new HashMap<>();
+        for (String key : keys) {
+            String value = SysParametersUtils.getStringValue(key);
+            if (StringUtils.isNotBlank(value)){
+                map.put(key,value);
+            }
+        }
+        if (map.size()!=keys.size()){
+            String serverAddr = SysParametersUtils.getStringValue("nacos.server-addr");
+            String dataId = SysParametersUtils.getStringValue("nacos.system-dataid");
+            String group = "CENTIT";
+            Properties nacosProperties = new Properties();
+            nacosProperties.put("serverAddr", serverAddr);
+            ConfigService configService = NacosFactory.createConfigService(nacosProperties);
+            String content = configService.getConfig(dataId, group, 60000);
+            if (StringUtils.isNotBlank(content)){
+                ByteArrayInputStream inputStream = new ByteArrayInputStream(content.getBytes());
+                Properties properties = new Properties();
+                properties.load(inputStream);
+                //优先使用配置文件中的属性
+                for (String key : keys) {
+                    String value = properties.getProperty(key);
+                    if (StringUtils.isNotBlank(value)){
+                        map.put(key,value);
+                    }
+                }
+            }
+        }
+        System.getProperties().putAll(map);
     }
 
 }
