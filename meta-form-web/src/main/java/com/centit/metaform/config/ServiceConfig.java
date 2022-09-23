217f0b857e76ccf84d8b70de81c7389abd8b51d8
zhf <zhf@centit.com>
2022-09-23 11:20:01 +0800

    整理代码

--- meta-form-web/src/main/java/com/centit/metaform/config/ServiceConfig.java
+++ meta-form-web/src/main/java/com/centit/metaform/config/ServiceConfig.java
@@ -4,20 +4,14 @@ import com.alibaba.nacos.api.annotation.NacosProperties;
 import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySources;
-import com.centit.framework.components.impl.NotificationCenterImpl;
 import com.centit.framework.config.SpringSecurityCasConfig;
 import com.centit.framework.config.SpringSecurityDaoConfig;
-import com.centit.framework.core.service.DataScopePowerManager;
-import com.centit.framework.core.service.impl.DataScopePowerManagerImpl;
 import com.centit.framework.jdbc.config.JdbcConfig;
-import com.centit.framework.model.adapter.NotificationCenter;
 import com.centit.framework.security.model.StandardPasswordEncoderImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.env.Environment;
 
 @Configuration
 @ComponentScan(basePackages = {"com.centit"},
@@ -32,28 +26,11 @@ import org.springframework.core.env.Environment;
 )
 public class ServiceConfig {
 
-    @Autowired
-    private Environment environment;
-
     @Bean(name = "passwordEncoder")
     public StandardPasswordEncoderImpl passwordEncoder() {
         return new StandardPasswordEncoderImpl();
     }
 
-    @Bean
-    public DataScopePowerManager queryDataScopeFilter() {
-        return new DataScopePowerManagerImpl();
-    }
-
-
-    @Bean
-    public NotificationCenter notificationCenter() {
-        NotificationCenterImpl notificationCenter = new NotificationCenterImpl();
-        notificationCenter.initDummyMsgSenders();
-        //notificationCenter.registerMessageSender("innerMsg",innerMessageManager);
-
-        return notificationCenter;
-    }
 
 //    @Bean
 //    public MapSessionRepository sessionRepository() {
