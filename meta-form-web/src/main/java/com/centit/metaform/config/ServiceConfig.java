3def1c64dfe0dbcafcd288edcd21a98752d68d18
codefan <codefan@centit.com>
2023-12-20 16:30:22 +0800

    添加配置bean

--- meta-form-web/src/main/java/com/centit/metaform/config/ServiceConfig.java
+++ meta-form-web/src/main/java/com/centit/metaform/config/ServiceConfig.java
@@ -10,12 +10,17 @@ import com.centit.framework.config.SpringSecurityDaoConfig;
 import com.centit.framework.jdbc.config.JdbcConfig;
 import com.centit.framework.model.adapter.NotificationCenter;
 import com.centit.framework.security.StandardPasswordEncoderImpl;
+import com.centit.search.service.ESServerConfig;
+import com.centit.support.algorithm.NumberBaseOpt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
 
 @Configuration
+//@PropertySource("classpath:system.properties")
 @ComponentScan(basePackages = {"com.centit"},
         excludeFilters = @ComponentScan.Filter(value = org.springframework.stereotype.Controller.class))
 @Import({
@@ -26,6 +31,9 @@ import org.springframework.context.annotation.Import;
 @NacosPropertySources({@NacosPropertySource(dataId = "${nacos.system-dataid}", groupId = "CENTIT", autoRefreshed = true)}
 )
 public class ServiceConfig {
+
+    @Autowired
+    Environment env;
 
     @Bean(name = "passwordEncoder")
     public StandardPasswordEncoderImpl passwordEncoder() {
@@ -38,5 +46,18 @@ public class ServiceConfig {
         notificationCenter.initDummyMsgSenders();
         ///notificationCenter.registerMessageSender("innerMsg",innerMessageManager);
         return notificationCenter;
+    }
+
+    @Bean
+    public ESServerConfig esServerConfig() {
+        ESServerConfig config = new ESServerConfig();
+        config.setServerHostIp(env.getProperty("elasticsearch.server.ip"));
+        config.setServerHostPort(env.getProperty("elasticsearch.server.port"));
+        config.setClusterName(env.getProperty("elasticsearch.server.cluster"));
+        config.setOsId(env.getProperty("elasticsearch.osId"));
+        config.setUsername(env.getProperty("elasticsearch.server.username"));
+        config.setPassword(env.getProperty("elasticsearch.server.password"));
+        config.setMinScore(NumberBaseOpt.parseFloat(env.getProperty("elasticsearch.filter.minScore"), 0.5F));
+        return config;
     }
 }
